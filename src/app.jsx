527e084c20617ec78c9477c0806c61f3cebd9ee6
Hassan ODeh <96985966+Hassano1696@users.noreply.github.com>
2026-07-09 11:13:01 +0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+
+import Dashboard from "./pages/Dashboard";
+import Sales from "./pages/Sales";
+import Inventory from "./pages/Inventory";
+import CashBox from "./pages/CashBox";
+import More from "./pages/More";
+
+import BottomNav from "./components/BottomNav";
+
 export default function App() {
-  return (
-    <div>
-      <h1>Athar ERP</h1>
-      <p>Frontend is working 🚀</p>
-    </div>
-  );
+
+  const [page,setPage]=useState("home");
+
+  return(
+    <>
+
+      {page==="home"&&<Dashboard/>}
+      {page==="sales"&&<Sales/>}
+      {page==="inventory"&&<Inventory/>}
+      {page==="cash"&&<CashBox/>}
+      {page==="more"&&<More/>}
+
+      <BottomNav page={page} setPage={setPage}/>
+
+    </>
+  )
+
 }
