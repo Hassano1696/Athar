c01f74a75cc6192c4f74d4d2fa37353fd730df49
Hassan ODeh <96985966+Hassano1696@users.noreply.github.com>
2026-07-09 11:13:30 +0300

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,1 +1,46 @@
+export default function BottomNav({page,setPage}){
 
+return(
+
+<div className="bottomNav">
+
+<button
+className={page==="home"?"active":""}
+onClick={()=>setPage("home")}
+>
+🏠
+</button>
+
+<button
+className={page==="sales"?"active":""}
+onClick={()=>setPage("sales")}
+>
+💰
+</button>
+
+<button
+className={page==="inventory"?"active":""}
+onClick={()=>setPage("inventory")}
+>
+📦
+</button>
+
+<button
+className={page==="cash"?"active":""}
+onClick={()=>setPage("cash")}
+>
+💵
+</button>
+
+<button
+className={page==="more"?"active":""}
+onClick={()=>setPage("more")}
+>
+☰
+</button>
+
+</div>
+
+)
+
+}
