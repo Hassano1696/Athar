19c5a2a67484cf8ab6a4ff20a875909edb1ba0d4
Hassan ODeh <96985966+Hassano1696@users.noreply.github.com>
2026-07-09 11:14:08 +0300

    Update Dashboard.jsX

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,1 +1,31 @@
+export default function Dashboard(){
 
+return(
+
+<div className="page">
+
+<h1>Dashboard</h1>
+
+<div className="card">
+الصندوق
+
+<h2>₪ 0</h2>
+</div>
+
+<div className="card">
+مبيعات اليوم
+
+<h2>₪ 0</h2>
+</div>
+
+<div className="card">
+ربح اليوم
+
+<h2>₪ 0</h2>
+</div>
+
+</div>
+
+)
+
+}
